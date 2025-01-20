f005762c068fac75019e0c4df3fe2a588c0ce9fe
Anton Semenov <antsemion@gmail.com>
2025-01-20 21:19:12 +0300

    Update keycloak

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -3,7 +3,28 @@ import Keycloak from 'keycloak-js';
 const keycloak = new Keycloak({
     url: 'http://localhost:9000/realms/devrealm/protocol/openid-connect/auth',
     realm: 'devrealm',
-    clientId: 'cards-public'
+    clientId: 'cards-public',
+    KeycloakResponseType: 'code',
+    onLoad: 'login-required'
 });
+
+keycloak.init({
+    onLoad: 'login-required',
+    responseType: "code",
+    silentCheckSsoRedirectUri: window.location.origin + "/silent-check-sso.html",
+    checkLoginIframe: false,
+    redirectUri: 'http://localhost:3000/',
+    pkceMethod: 'S256'
+})
+    .then(authenticated => {
+        if (authenticated) {
+            console.log('Authenticated');
+        } else {
+            console.warn('Not authenticated');
+        }
+    })
+    .catch(err => {
+        console.error('Failed to initialize Keycloak', err);
+    });
 
 export default keycloak;
