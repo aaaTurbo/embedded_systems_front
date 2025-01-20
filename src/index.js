3b0d7cbf541924ef1a9ab0cb23a88e2cf3e2f416
Anton Semenov <antsemion@gmail.com>
2025-01-20 18:58:41 +0300

    Update keycloak compatibility

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './store';
-import keycloak from "./config/keycloack";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider store={store}>
-        <ReactKeycloakProvider authClient={keycloak}>
             <React.StrictMode>
                 <App/>
             </React.StrictMode>
-        </ReactKeycloakProvider>
     </Provider>,
 );
