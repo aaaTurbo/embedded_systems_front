240e58788a9506a90d24fb7f03d87089ac39d4a6
Anton Semenov <antsemion@gmail.com>
2025-01-20 19:00:12 +0300

    Update keycloak usage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,22 +1,16 @@
 import CardsTable from "../components/CardsTable";
 import EventsTable from "../components/EventsTable";
 import ControlPanel from "../components/ControlPanel";
-import useWebSocket from "../config/ws";
-import { useKeycloak } from '@react-keycloak/web';
 import {useTranslation} from "react-i18next";
+import keycloak from "../config/keycloak";
 
 export default function MainPage() {
 
     const {t} = useTranslation();
 
-    const { keycloak } = useKeycloak();
-
     if (!keycloak.authenticated) {
         return <div>{t("app.notAuthenticated")}</div>;
     }
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useWebSocket();
 
     return (<>
         <div className="container" id="main">
