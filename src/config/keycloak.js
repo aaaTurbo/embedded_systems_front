b05ae5aa8c3df6636011cb3ac22508cdb1787e1e
Anton Semenov <antsemion@gmail.com>
2025-01-20 21:06:18 +0300

    Update url

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:9000',
+    url: 'http://localhost:9000/realms/devrealm/protocol/openid-connect/auth',
     realm: 'devrealm',
     clientId: 'cards-public'
 });
